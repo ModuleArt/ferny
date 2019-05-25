691a69d1d446f8b419f1a55a7c5871b5889976c0
Beelink <volynko.ua@gmail.com>
2019-05-26 01:32:46 +0300

    ArrowFlat UI Design 2.1 / New version Beta v1.2.1

--- js/navbar.js
+++ js/navbar.js
@@ -1,22 +1,33 @@
 function toggleMenu() {
-  var btns = document.getElementsByClassName('navbar-btn');
-  var links = document.getElementById('navbar-links');
+  // var btns = document.getElementsByClassName('navbar-btn');
+  // var links = document.getElementById('navbar-links');
+  // var menuIcon = document.getElementById('navbar-menu-btn').getElementsByTagName('img')[0];
+  // console.log(menuIcon);
+
+  // if(links.style.display == "block") {
+  //   for(var i = 0; i < btns.length; i++) {
+  //     btns[i].style.display = "none";
+  //   }
+
+  //   links.style.display = "none";
+  //   menuIcon.src = "imgs/icons/menu.png";
+  // } else {
+  //   for(var i = 0; i < btns.length; i++) {
+  //     btns[i].style.display = "block";
+  //   }
+
+  //   links.style.display = "block";
+  //   menuIcon.src = "imgs/icons/close.png";
+  // }
+
   var menuIcon = document.getElementById('navbar-menu-btn').getElementsByTagName('img')[0];
-  console.log(menuIcon);
+  var navbar = document.getElementById('navbar');
 
-  if(links.style.display == "block") {
-    for(var i = 0; i < btns.length; i++) {
-      btns[i].style.display = "none";
-    }
-
-    links.style.display = "none";
+  if(navbar.classList.contains('show')) {
+    navbar.classList.remove('show');
     menuIcon.src = "imgs/icons/menu.png";
   } else {
-    for(var i = 0; i < btns.length; i++) {
-      btns[i].style.display = "block";
-    }
-
-    links.style.display = "block";
+    navbar.classList.add('show');
     menuIcon.src = "imgs/icons/close.png";
   }
 }
